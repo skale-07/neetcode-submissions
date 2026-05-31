41d2debcf699f96003b1f760e0da0c7c12161b58
skale-07 <136627895+skale-07@users.noreply.github.com>
2026-05-31 14:47:07 -0700

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+        output = []
+
+        for word in strs:
+            sort_word = "".join(sorted(word))
+            if sort_word not in anagrams:
+                anagrams[sort_word] = [word]
+            else:
+                anagrams[sort_word].append(word)
+
+        for key, value in anagrams.items():
+            output.append(value)
+
+        return output 
