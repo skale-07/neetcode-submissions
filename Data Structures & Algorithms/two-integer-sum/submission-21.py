2cefc74b4d8bfb7e867e6e31764d7e118a896017
skale-07 <136627895+skale-07@users.noreply.github.com>
2026-08-30 10:14:51 -0700

    Add: two-integer-sum - submission-21

---
+++ Data Structures & Algorithms/two-integer-sum/submission-21.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hMap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hMap:
+                return sorted([i, hMap[diff]])
+            hMap[n] = i
